efb47f56010168a16765066cd88034254ed9d8ce
Meta Engineer <m@plankc.com>
2020-11-04 09:30:26 -0800

    make emergency-cleanup.sh do more useful things

--- examples/istio/emergency-cleanup.sh
+++ examples/istio/emergency-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd ../.. && ./buildflowd.sh && docker build -t healthd -f deploy/Dockerfile.healthd .
-nsp default
+cd ../.. && docker build -t flowd -f deploy/Dockerfile.flowd . && docker build -t healthd -f deploy/Dockerfile.healthd .
+kubectl config set-context --current --namespace=default
 kubectl delete --ignore-not-found services collect secret-sauce profit xgateway-did-apply
 kubectl delete --ignore-not-found deployments collect secret-sauce profit xgateway-did-apply
 kubectl delete --ignore-not-found serviceaccounts collect secret-sauce profit xgateway-did-apply
