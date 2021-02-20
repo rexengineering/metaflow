1e304c32568cb8756eb325fdccd463b487beec5f
Meta Engineer <m@plankc.com>
2021-02-19 18:09:06 -0600

    REXFLOW-122 : Add verb to force health check Added two access points to healthd: /probe and /probe/<workflow_id> Added verb probe to flowctl:
    flowctl probe [workflow_id ...]
    
    The default behavior is to perform health probes on all workflows,
    otherwise only the workflows specified are probed.

---
+++ flowd/handlers/handle_probe.py
@@ -0,0 +1,33 @@
+import requests
+
+from flowlib import flow_pb2
+from flowlib.constants import WorkflowKeys, WorkflowInstanceKeys
+
+
+class ProbeHandlers:
+    def __init__(self):
+        self.healthd_host = 'healthd'
+        self.healthd_port = 5050
+        self.url = f'http://{self.healthd_host}:{self.healthd_port}/probe'
+
+    def handle_single_deployment(self, deployment_id):
+        return requests.get(f'{self.url}/{deployment_id}').json()
+
+    def handle_some_deployments(self, ids):
+        result = dict()
+        for deployment_id in ids:
+            result[deployment_id] = self.handle_single_deployment(deployment_id)
+        return result
+
+    def handle_all_deployments(self):
+        return requests.get(self.url).json()
+
+def handler(request):
+    result = None
+    handlers = ProbeHandlers()
+    if request.ids:
+        result = handlers.handle_some_deployments(request.ids)
+    else:
+        result = handlers.handle_all_deployments()
+    
+    return result
