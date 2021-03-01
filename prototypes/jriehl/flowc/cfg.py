c1fad65eec8d8fb96e5da84589c3ed79e47314f8
Meta Engineer <m@plankc.com>
2021-03-01 14:19:44 -0600

    Adds additional logic to handle unterminated blocks, and dead code.

--- prototypes/jriehl/flowc/cfg.py
+++ prototypes/jriehl/flowc/cfg.py
@@ -43,7 +43,7 @@ def dump_blocks(blocks: List[CFGBlock]) -> str:
 
 class CFGTransfomer(ast.NodeVisitor):
     function: ast.FunctionDef
-    blocks: List[CFGBlock]
+    blocks: List[Optional[CFGBlock]]
     current_block: Optional[CFGBlock]
 
     def __init__(self, function: ast.FunctionDef):
@@ -64,20 +64,53 @@ class CFGTransfomer(ast.NodeVisitor):
         raise NotImplementedError('Unsupported Python construct.')
 
     def generic_stmt(self, node: ast.stmt):
-        assert self.current_block is not None, 'Internal error.'
+        assert self.current_block is not None, 'Internal error: the CFG ' \
+            'transformer is not currently pointing at a basic block.'
         self.current_block.statements.append(node)
 
+    def _eliminate_dead_code(self):
+        in_counts = {0: 1}
+        in_counts.update((block.index, 0) for block in self.blocks[1:])
+        for block in self.blocks[1:]:
+            terminal = block.terminal
+            assert terminal is not None, 'Internal error: unterminated ' \
+                'basic block detected during dead code elimination.'
+            if isinstance(terminal, CFGJump):
+                in_counts[terminal.branch] += 1
+            elif isinstance(terminal, CFGBranch):
+                in_counts[terminal.true_branch] += 1
+                in_counts[terminal.false_branch] += 1
+        for block_index, in_count in in_counts.items():
+            if in_count < 1:
+                if block_index == len(self.blocks) - 1:
+                    self.blocks.pop()
+                else:
+                    self.blocks[block_index] = None
+
     def visit_FunctionDef(self, node: ast.FunctionDef):
-        assert len(self.blocks) == 0, 'Nested functions in a workflow are not supported.'
+        assert len(self.blocks) == 0, 'Nested functions in a workflow are ' \
+            'not supported.'
         self.current_block = self.new_block()
         self.generic_visit(node)
+        assert self.current_block == self.blocks[-1], 'Internal error: the ' \
+            'CFG transformer is not pointing at the tail block ' \
+            f'({self.current_block}).'
+        if not isinstance(self.current_block.terminal, CFGReturn):
+            assert self.current_block.terminal is None, 'Internal error: ' \
+                'tail block in function does not end in a return.'
+            self.current_block.terminal = CFGReturn(ast.Return(value=None))
+        self.current_block = None
+        self._eliminate_dead_code()
 
     visit_AsyncFunctionDef = unsupported
     visit_ClassDef = unsupported
 
     def visit_Return(self, node: ast.Return):
+        assert self.current_block.terminal is None, 'Internal error: I have ' \
+            'hit a return statement within a basic block that is already ' \
+            'terminated.'
         self.current_block.terminal = CFGReturn(node)
-        self.current_block = None
+        self.current_block = self.new_block()
 
     visit_Delete = generic_stmt
     visit_Assign = generic_stmt
--- prototypes/jriehl/flowc/tests/test_cfg.py
+++ prototypes/jriehl/flowc/tests/test_cfg.py
@@ -1,8 +1,21 @@
+import ast
 import unittest
 
 from .. import cfg, flowclib
 from .test_flowclib import BRANCH_PATH
 
+
+NO_ORELSE_SRC = '''
+def no_orelse():
+    if some_predicate():
+        do_something()
+'''
+
+DEAD_CODE_SRC = '''
+def dead_code():
+    return 42
+    pass
+'''
 
 class TestCFG(unittest.TestCase):
     def test_branch(self):
@@ -12,6 +25,26 @@ class TestCFG(unittest.TestCase):
         visitor = cfg.CFGTransfomer(toplevel.workflow)
         blocks = visitor.get_cfg()
         self.assertEqual(len(blocks), 4)
+
+    @staticmethod
+    def _parse(src: str) -> cfg.CFGTransfomer:
+        tree = ast.parse(src, '<unit test input>', 'exec')
+        function_def = tree.body[0]
+        assert isinstance(function_def, ast.FunctionDef)
+        cfg_visitor = cfg.CFGTransfomer(function_def)
+        return cfg_visitor
+
+    def test_empty(self):
+        cfg_visitor = self._parse(NO_ORELSE_SRC)
+        cfg_blocks = cfg_visitor.get_cfg()
+        self.assertEqual(len(cfg_blocks), 3)
+        self.assertIsNotNone(cfg_blocks[-1].terminal)
+
+    def test_dead_code(self):
+        cfg_visitor = self._parse(DEAD_CODE_SRC)
+        cfg_blocks = cfg_visitor.get_cfg()
+        self.assertEqual(len(cfg_blocks), 1)
+        self.assertIsNotNone(cfg_blocks[-1].terminal)
 
 
 if __name__ == '__main__':
