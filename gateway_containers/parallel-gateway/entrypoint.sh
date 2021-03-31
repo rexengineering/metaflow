f528ca07e6c844170162d6cbc7cac422b1582ced
Meta Engineer <m@plankc.com>
2021-03-31 09:00:49 -0700

    pr fixes

--- gateway_containers/parallel-gateway/entrypoint.sh
+++ gateway_containers/parallel-gateway/entrypoint.sh
@@ -3,4 +3,4 @@
 . activate parallel-gateway
 export PYTHONPATH=${PYTHONPATH}:.
 
-exec gunicorn -k gthread --threads 8 -b :5000 --reload --timeout 120 --forwarded-allow-ips="*" --log-level=debug --error-logfile - --access-logfile - 'code.routes:app'
+exec gunicorn -k gthread --threads 8 -b :5000 --reload --timeout 120 --forwarded-allow-ips="*" --log-level=info --error-logfile - --access-logfile - 'code.routes:app'
