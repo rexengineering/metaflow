d3dee0524b050ea86aa0dc6e8bcf09328d8a64ad
Meta Engineer <m@plankc.com>
2021-04-24 18:17:53 -0500

    flowctl loads field description JSON file, converts it to yaml, and inserts it into the annotation for the user task.

--- flowctl/actions/apply_action.py
+++ flowctl/actions/apply_action.py
@@ -1,9 +1,12 @@
 import argparse
+import json
 import logging
-
+import os
 import xmltodict
+import yaml
 
 from flowlib import flow_pb2
+from flowlib import bpmn_util
 from flowlib.flowd_utils import get_flowd_connection
 
 __help__ = 'apply sufficiently annotated BPMN file(s)'
@@ -29,19 +32,37 @@ def __refine_args__(parser: argparse.ArgumentParser):
     return parser
 
 
-def process_specification(specification):
-    '''process_specification(specification)
+def process_specification(spec_file):
+    '''process_specification(spec_file)
     Given a BPMN specification (this can either be a file or string), perform
     rudimentary validation and cleanup of the document.  Returns a string with
     the resulting XML.
     '''
-    spec = xmltodict.parse(specification)
+    spec = xmltodict.parse(spec_file)
     definition = spec['bpmn:definitions']
     definition_child_count = len([key for key in definition if key[0] != '@'])
     assert definition_child_count == 2, \
         f'Unexpected child count (got {definition_child_count}, not 2).'
+    '''
+    Check for user tasks, and if found, check for form specification files and
+    import them inline.
+    '''
+    process = definition['bpmn:process']
+    if process and 'bpmn:userTask' in process.keys():
+        for task in process['bpmn:userTask']:
+            for annot,text in bpmn_util.get_annotations(process, task['@id']):
+                if 'rexflow' in text and 'fields' in text['rexflow']:
+                    ''' Load the field description JSON from the provided file. If the file name
+                    start with anything other than '/', then assume the file is in the same folder
+                    as the source BPMN file.'''
+                    fspec = text['rexflow']['fields']['file']
+                    if not fspec.startswith('/'):
+                        fspec = '/'.join([os.path.dirname(spec_file.name),fspec])
+                    with open(fspec, 'r') as fd:
+                        data = fd.read().replace('\n','')
+                        text['rexflow']['fields']['desc'] = json.loads(data)
+                        annot['bpmn:text'] = yaml.dump(text)
     return xmltodict.unparse(spec)
-
 
 def apply_action(namespace: argparse.Namespace, *args, **kws):
     '''apply_action(namespace)
--- flowlib/user_task.py
+++ flowlib/user_task.py
@@ -1,13 +1,14 @@
 '''
 Implements the BPMNUserTask class, which inherits from BPMNComponent.
 '''
+import json
+import logging
 
 from collections import OrderedDict
 from flowlib.config import UI_BRIDGE_NAME, UI_BRIDGE_INIT_PATH, UI_BRIDGE_PORT
 from typing import Any, Mapping
 
-from flowlib.bpmn_util import BPMNComponent, WorkflowProperties
-
+from flowlib.bpmn_util import BPMNComponent, WorkflowProperties, get_annotations
 
 class BPMNUserTask(BPMNComponent):
     def __init__(self, user_task: OrderedDict, process: OrderedDict, global_props: WorkflowProperties):
@@ -19,6 +20,12 @@ class BPMNUserTask(BPMNComponent):
         self.service_properties._host = UI_BRIDGE_NAME
         self.service_properties._port = UI_BRIDGE_PORT
         self.call_properties._path = UI_BRIDGE_INIT_PATH
+        self.field_desc = None
+
+        for annot,text in get_annotations(process, self.id):
+            if 'rexflow' in text and 'fields' in text['rexflow'] and 'desc' in text['rexflow']['fields']:
+                self.field_desc = text['rexflow']['fields']['desc']
+                break
 
     def to_kubernetes(self, id_hash, component_map: Mapping[str, Any],
                       digraph: OrderedDict, sequence_flow_table: Mapping[str, Any]) -> list:
