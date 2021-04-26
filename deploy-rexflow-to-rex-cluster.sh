d3b2bd29338ae358b5956e69e99a9e078ef26d6a
Meta Engineer <m@plankc.com>
2021-04-26 10:40:19 -0500

    oops

--- deploy-rexflow-to-rex-cluster.sh
+++ deploy-rexflow-to-rex-cluster.sh
@@ -3,9 +3,8 @@
 set -ex
 rex-k8s-context $1
 
-#   rex-ecr-tag --repo rex/rexflow-$image --image b-master --move --tag $1
-for image in {flowd,healthd,throw-gateway,catch-gateway,exclusive-gateway} ; do
-  rex-ecr-tag --repo rex/rexflow-$image --image 0.0.0 --move --tag $1;
+for image in {flowd,healthd,throw-gateway,catch-gateway,exclusive-gateway,parallel-gateway} ; do
+  rex-ecr-tag --repo rex/rexflow-$image --image b-master --move --tag $1;
 done
 
 rex-k8s-context $1:rexflow
