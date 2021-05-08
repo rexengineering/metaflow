b980a8ed8e3fcbfe4cd886875cb64b13dd2a2e1e
Meta Engineer <m@plankc.com>
2021-05-08 18:56:45 +0000

    Merged in feature/REXFLOW-189-update-setuptools (pull request #120)
    Feature/REXFLOW-189 update setuptools
    
    * Makes repository conform to PEP-517, updates setup.py to only build and package flowlib.
    
    * Updates .gitignore to ignore any Python egg-info subdirectories.
    
    * Adds library dependencies for flowlib build.

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 import setuptools
+from subprocess import check_output
+
+git_version = check_output("git describe --abbrev=4 --dirty --always".split()).decode('utf-8').strip()
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rexflow", # Replace with your own username
-    version="0.0.1",
+    name="flowlib",  # NOTE: Hopefully this is temporary; see REXFLOW-190.
+    version=git_version,
     author="REXFlow Developers",
     author_email="engineering@rexchange.com",
-    description="A framework for workflow orchestration",
-    long_description=long_description,
+    description="A utility library for workflow orchestration",
+    long_description=long_description,  # TODO: Make this flowlib specific...
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/rexdev/rexflow",
-    packages=setuptools.find_packages(),
+    packages=['flowlib', 'flowlib.configs'],
     classifiers=[
         "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        'boto3',
+        'confluent-kafka',
+        'etcd3',
+        'grpcio',
+        'quart',
+        'xmltodict',
     ],
     python_requires='>=3.8',
 )
